9c1ba59fb82b1ffe598c7fc0c0ecce1fb0c4d6f2
Philippe Llerena <philippe.llerena@gmail.com>
2026-04-27 11:33:30 +0200

    fix(start.sh): update database filename from tpms_010.db to tpms_011.db

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 cargo build --release
-./target/release/tpms-sniffer --json | ./target/release/tpms-tracker --db tpms_010.db --serve 0.0.0.0:8080 --log-level debug --log-file ~/logs/tpms-tracker.log
+./target/release/tpms-sniffer --json | ./target/release/tpms-tracker --db tpms_011.db --serve 0.0.0.0:8080 --log-level debug --log-file ~/logs/tpms-tracker.log
